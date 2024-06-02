7b07183a3ffc8627f46abb9e1126b6ec936212f0
Igor <qwertyhock@gmail.com>
2024-06-02 23:47:48 +0300

    initial structure

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/index.scss'
+import SomeComponents from 'entities/SomeComponents/SomeComponents'
+
+const App = () => {
+	return (
+		<div className='main'>
+			<SomeComponents />
+		</div>
+	)
+}
+
+export default App
